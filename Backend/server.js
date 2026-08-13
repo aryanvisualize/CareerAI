c7bb2a1e8c1aa9c70e875e0efc782574f709411d
aryanvisualize <aryanvisualize@gmail.com>
2026-08-14 01:43:01 +0530

    feat: add interview report model and AI service for generating interview reports

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = require("./src/app");
 const connectToDB = require("./src/config/database.js");
 
 connectToDB();
+
 app.listen(3000, ()=>{
-    console.log(`Server is running on port 3000`);
+    console.log(`Server is running on port 3000`); 
 })
