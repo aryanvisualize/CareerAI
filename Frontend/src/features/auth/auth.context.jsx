4747288aeff3f2144a52325dae04172ea1e5bf34
aryanvisualize <aryanvisualize@gmail.com>
2026-08-13 23:31:23 +0530

    refactor: clean up authentication context and hooks, reintroduce user fetching logic

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,26 +1,17 @@
-import { createContext, useState } from "react";
-import {getMe} from "../api/auth.api.js"
-import { useEffect } from "react";
+import { createContext,useState } from "react";
 
 export const AuthContext = createContext()
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => { 
+
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const getAndSetUser = async () => {
-            const data = await getMe()
-            setUser(data.user)
-            setLoading(false)
-        }
-
-        getAndSetUser();    
-    }, []);
-
     return (
-        <AuthContext.Provider value={{user, setUser, loading, setLoading}} >
+        <AuthContext.Provider value={{user,setUser,loading,setLoading}} >
             {children}
         </AuthContext.Provider>
     )
+
+    
 }
--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,53 +1,68 @@
-import {useContext} from "react";
-import { AuthContext } from "../auth.context.jsx";
-import {login, register, logout, getMe} from "../services/auth.api.js"
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+
 
 export const useAuth = () => {
+
     const context = useContext(AuthContext)
-    const {user, setUser, loading, setLoading} = context
+    const { user, setUser, loading, setLoading } = context
 
-    const handleLogin = async ({email, password}) => {
+
+    const handleLogin = async ({ email, password }) => {
         setLoading(true)
-        try{
-            const data = await login({email, password})
+        try {
+            const data = await login({ email, password })
             setUser(data.user)
+        } catch (err) {
+
+        } finally {
+            setLoading(false)
         }
-        catch(err) {
+    }
+
+    const handleRegister = async ({ username, email, password }) => {
+        setLoading(true)
+        try {
+            const data = await register({ username, email, password })
+            setUser(data.user)
+        } catch (err) {
             console.log(err);
-        }
-        finally{
-            setLoading(false)
-        }
-        const data = await login({email, password})
-        setUser(data.user)
-        setLoading(false)
-    }
-
-
-    const handleRegister = async ({username, email, password}) => {
-        setLoading(true)
-        try{
-            const data = await register({username, email, password})
-            setUser(data.user)
-        } catch(err){
-            console.log(err)
-        }
-        finally{
+        } finally {
             setLoading(false)
         }
     }
 
-    const handleLogout = async ()=> {
+    const handleLogout = async () => {
         setLoading(true)
-        try{
+        try {
             const data = await logout()
             setUser(null)
-        } catch(err){
-            console.log(err)
-        }finally{ 
+        } catch (err) {
+            console.log(err);
+        } finally {
             setLoading(false)
         }
     }
 
-    return {user, loading, handleRegister, handleLogin, handleLogout}
+    useEffect(() => {
+
+        const getAndSetUser = async () => {
+            try {
+
+                const data = await getMe()
+                setUser(data.user)
+            } catch (err) {
+                console.log(err);
+             } finally {
+                setLoading(false)
+            }
+        }
+
+        getAndSetUser()
+
+    }, [])
+
+    return { user, loading, handleRegister, handleLogin, handleLogout }
 }
