2490d4f6007b78ce04a0504811101ad9d766b065
aryanvisualize <aryanvisualize@gmail.com>
2026-08-16 03:23:31 +0530

    feat: add interview context and pages for generating and viewing interview plans
    - Created InterviewContext for managing interview-related state.
    - Implemented Home page for generating interview reports based on job and self descriptions.
    - Developed Interview page to display generated interview plans with technical and behavioral questions, and a preparation roadmap.
    - Added API services for generating and retrieving interview reports.
    - Styled Home and Interview pages with SCSS for improved UI/UX.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes.jsx";
 import { AuthProvider } from "./features/auth/auth.context.jsx";
+import { InterviewProvider } from "./features/interview/interview.context.jsx";
 
 function App() {
 
   return (
-    <AuthProvider>
-        <RouterProvider router={router}/>
-    </AuthProvider>
+    <InterviewProvider>
+        <AuthProvider>
+            <RouterProvider router={router}/>
+        </AuthProvider>
+    </InterviewProvider>
+    
   )
 }
 
