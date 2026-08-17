26d67f456457bb9192b6d347b60976e9c580cd94
aryanvisualize <aryanvisualize@gmail.com>
2026-08-17 16:53:14 +0530

    Frontend deployment setup

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
-})
+});
 
-export async function register({username, email, password}) {
-    try{
+export async function register({ username, email, password }) {
+    try {
         const response = await api.post('/api/auth/register', {
-            username, email, password
-        })
+            username,
+            email,
+            password
+        });
 
         return response.data;
-    }
-    catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
 
@@ -38,20 +40,22 @@ export async function login({ email, password }) {
 
 export async function logout() {
     try {
-        const response = await api.post("/api/auth/logout")
+        const response = await api.post("/api/auth/logout");
         return response.data;
     } catch (error) {
-        console.error("Logout failed:", error.response?.data || error.message)
+        console.error(
+            "Logout failed:",
+            error.response?.data || error.message
+        );
+
         throw error;
     }
 }
 
 export async function getMe() {
     try {
-        const response = await api.get("/api/auth/get-me")
-
+        const response = await api.get("/api/auth/get-me");
         return response.data;
-
     } catch (error) {
         throw error;
     }
